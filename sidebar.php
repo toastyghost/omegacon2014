581e299ca4d2965f65dd1e2646ac13e3fa9ee9e1
Josh Clark <toastyghost@live.com>
2014-05-12 09:43:09 +0100

    re-enabled sidebar for widget testing

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-<?php /*<div id="sidebar1" class="sidebar large-4 medium-4 columns" role="complementary">
+<div id="sidebar1" class="sidebar large-4 medium-4 columns" role="complementary">
 
 	<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
 
@@ -14,4 +14,4 @@
 
 	<?php endif; ?>
 
-</div>*/ ?>
+</div>
