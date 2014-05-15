ee10c6806b99bc4fe97d4c18bd328c5a4e0bbe34
Josh Clark <toastyghost@live.com>
2014-05-15 04:34:39 +0100

    messed w/ g+ follow widget some more & added seamless planet footer art

--- header.php
+++ header.php
@@ -64,14 +64,8 @@
 							<div id="social-follow-widgets">
 								<div class="fb-like" data-href="https://facebook.com/omegaconVA" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
 								
+								<script type="text/javascript" src="https://apis.google.com/js/platform.js"></script>
 								<div class="g-follow" data-annotation="bubble" data-height="20" data-href="//plus.google.com/u/0/108459089226316214541" data-rel="publisher"></div>
-								<script type="text/javascript">
-									(function() {
-										var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-										po.src = 'https://apis.google.com/js/platform.js';
-										var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-									})();
-								</script>
 								
 								<a href="https://twitter.com/omegaconVA" class="twitter-follow-button" data-show-count="true" data-show-screen-name="false" data-lang="en">Follow</a>
 								<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
